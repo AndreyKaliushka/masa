45bc869062223804b3aeaab5871d952d26f544c9
Andrey <kaliushka890@gmail.com>
2023-08-30 01:05:36 +0700

    rewrites code

--- source/js/blocks/news-sorter.js
+++ source/js/blocks/news-sorter.js
@@ -1,16 +1,17 @@
 const newsButtons = document.querySelectorAll('.news__button');
+const newsButtonsWrapper = document.querySelector('.news__buttons-wrapper');
+
+const setUpNewsButton = (evt) => {
+  const activeButton = evt.target.closest('.news__button');
+  if (activeButton.classList.contains('news__button--active')) {
+    return;
+  }
+  newsButtons.forEach((button) => button.classList.remove('news__button--active'));
+  activeButton.classList.add('news__button--active');
+};
 
 const setNewsButton = () => {
-  if (document.body.contains(document.querySelector('.news'))) {
-    newsButtons.forEach((newsButton) => newsButton.addEventListener('click', (evt) => {
-      newsButtons.forEach((newsButtonActive) => {
-        newsButtonActive.classList.remove('news__button--active');
-      });
-      if (evt) {
-        newsButton.classList.add('news__button--active');
-      }
-    }));
-  }
+  newsButtonsWrapper.addEventListener(('click'), setUpNewsButton);
 };
 
 export {setNewsButton};
